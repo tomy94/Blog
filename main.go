cacae721da341aed31966a03e2f6b349aa58359f
Tomass Dzenis <tomass.dzenis@leta.lv>
2014-04-17 19:42:18 +0300

    adds some changes to main.go

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"github.com/go-martini/martini"
+	// "github.com/russross/blackfriday"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"net/http"
+)
 
+func PanicIf(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func setupDB() *sql.DB {
+	db, e := sql.Open("postgres", "dbname=Blog username=postgres password=postgres sslmode=disable")
+	PanicIf(e)
+}
+
+func main() {
+	m := martini.Classic()
+
+	m.Get("/", func(rw http.ResponseWriter, r *http.Request) string {
+		return "Hello world!"
+	})
+
+	m.Run()
 }
