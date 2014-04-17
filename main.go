27144165e9fed7a3eb17077cd51d602c0352f113
Tomass Dzenis <tomass.dzenis@leta.lv>
2014-04-17 21:39:57 +0300

    sessions added

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	// "fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/sessions"
 	// "github.com/russross/blackfriday"
 	"database/sql"
 	_ "github.com/lib/pq"
@@ -14,6 +15,10 @@ type Article struct {
 	Title  string
 	Author string
 	Body   string
+}
+
+type User struct {
+	Username string
 }
 
 func PanicIf(e error) {
@@ -30,35 +35,63 @@ func SetupDB() *sql.DB {
 
 func main() {
 	m := martini.Classic()
+
+	//Sessions
+	store := sessions.NewCookieStore([]byte("thisIsTheSecret"))
+
 	m.Map(SetupDB())
+	m.Use(sessions.Sessions("BlogSession", store))
 	m.Use(render.Renderer(render.Options{
 		Layout: "layout",
 	}))
 
-	m.Get("/", FirstPage)
-	m.Post("/login", PostLogin)
+	m.Get("/", ShowArticles)
+	m.Get("/login", Login)
+	m.Get("/logout", LogOut)
+	m.Post("/authorize", PostLogin)
 	m.Get("/articles", ShowArticles)
-	m.Get("/create", NewArticle)
+	m.Get("/create", RequireLogin, NewArticle)
 	m.Post("/article", CreateArticle)
 
 	m.Run()
 }
 
-func FirstPage(ren render.Render) {
+func LogOut(s sessions.Session) string {
+	s.Delete("userId")
+	return "logged out"
+}
+
+func RequireLogin(rw http.ResponseWriter, req *http.Request, s sessions.Session, db *sql.DB, c martini.Context) {
+
+	user := &User{}
+	e := db.QueryRow(`SELECT username FROM users WHERE id=$1`, s.Get("userId")).Scan(&user.Username)
+	if e != nil {
+		http.Redirect(rw, req, "/login", http.StatusFound)
+		return
+	}
+
+	//map the user to the context
+	c.Map(user)
+}
+
+func Login(ren render.Render) {
 	ren.HTML(200, "login", nil)
 }
 
-func PostLogin(req *http.Request, db *sql.DB) (int, string) {
+func PostLogin(req *http.Request, db *sql.DB, s sessions.Session, ren render.Render) {
 	var id string
 
 	username, password := req.FormValue("username"), req.FormValue("password")
 	e := db.QueryRow("SELECT id FROM users WHERE username=$1 AND pwd=$2", username, password).Scan(&id)
 
 	if e != nil {
-		return 401, "Not Authorized"
+		return
 	}
 
-	return 200, "User id is: " + id
+	//set the userId in the session
+	s.Set("userId", id)
+
+	ren.Redirect("/articles")
 }
 
 func NewArticle(ren render.Render) {
